65622ada71a64a3341336e326fb468b29101b038
Jacpc <tonho_kun@ymail.com>
2025-01-22 06:11:35 -0300

    zero tolerance zone

--- src/main/java/ufrn/imd/service/DeviceAnalyticsService.java
+++ src/main/java/ufrn/imd/service/DeviceAnalyticsService.java
@@ -16,6 +16,7 @@ public class DeviceAnalyticsService {
 		this.deviceStatusServiceClient = deviceStatusServiceClient;
 	}
 	
+	
 	private List<DeviceStatus> fetchAllDeviceStatuses() {
         return deviceStatusServiceClient.getAllDeviceStatuses();
     }
